54d5c02eb9c78a858d427d7786c4ec29abe938f3
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-09-22 23:04:38 +0530

    pushed build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import FrontPage from "@/Components/FrontPage";
-import ContactOwner from "@/Components/ContactOwner";
 export default function Home() {
   return (
     <div>
       <FrontPage/>
       <Footer/>
-      {/* <ContactOwner></ContactOwner> */}
     </div>
   );
 }
